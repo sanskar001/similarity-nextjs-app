6a87f12c994473ed72fbfec3123ed92d8148edc6
sanskar001 <sanskarm001@gmail.com>
2024-01-20 19:59:22 +0530

    FEAT: add Navbar component with respective components and UI button and dropdown menu component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 import Paragraph from "../components/UI/Paragraph";
 
 export default function Home() {
-  return (
-    <div>
-      <Paragraph className="border border-black" size="sm">
-        Hello
-      </Paragraph>
-    </div>
-  );
+  return <main>Hello</main>;
 }
